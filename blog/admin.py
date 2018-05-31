e0500ab1697beed35bc99f6c838fb647acd801f4
zhoust <zhoust@guahao.com>
2018-06-01 00:17:47 +0800

    upload blog base

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import BlogArticles
+# Register your models here.
+
+admin.site.register(BlogArticles)
---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+# Create your models here.
+
+class BlogArticles(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, related_name="blog_posts", on_delete=models.CASCADE)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ("-publish",)
+
+    def __str__(self):
+        return self.title
