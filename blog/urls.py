2cabf4b00d24066b4e9dcb98555125a41ef21256
zhoust <zhoust@guahao.com>
2018-06-02 00:00:47 +0800

    update blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.blog_title, name='blog_titles')
+    re_path(r'^$', views.blog_title, name='blog_titles'),
+    re_path(r'(?P<article_id>\d)/$', views.blog_article, name='blog_article')
 ]
